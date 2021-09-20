e6a3fd8d62551ee03e29751ec7bdb5a9f1b6ee2c
Ayush Mondal <mondalayush555@gmail.com>
2021-09-20 18:41:34 +0530

    Sum java file is commited

---
+++ sum.java
@@ -0,0 +1,13 @@
+package LetsLearnGit;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+        System.out.println(sum(a, b));
+    }
+    
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+}
